7aff2d3a3fe85055e7cae911c2cc5ccddb4f3344
Alf Müller <4lf-mueller@gmx.de>
2019-10-07 23:03:20 +0200

    Update for TTT2 v0.5.7b

--- lua/terrortown/entities/roles/jackal/shared.lua
+++ lua/terrortown/entities/roles/jackal/shared.lua
@@ -11,28 +11,29 @@ roles.InitCustomTeam(ROLE.name, { -- this creates the var "TEAM_JACKAL"
 	color = Color(100, 190, 205, 255)
 })
 
-ROLE.Base = "ttt_role_base"
-
-ROLE.color = Color(100, 190, 205, 255) -- ...
-ROLE.dkcolor = Color(36, 134, 152, 255) -- ...
-ROLE.bgcolor = Color(255, 188, 121, 255) -- ...
-ROLE.abbr = "jack" -- abbreviation
-ROLE.defaultTeam = TEAM_JACKAL -- the team name: roles with same team name are working together
-ROLE.defaultEquipment = JACKAL_EQUIPMENT -- here you can set up your own default equipment
-ROLE.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
-ROLE.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
-ROLE.scoreTeamKillsMultiplier = -8 -- multiplier for teamkill
-ROLE.fallbackTable = {}
-ROLE.traitorCreditAward = true -- will receive credits on kill like a traitor
-
-ROLE.conVarData = {
-	pct = 0.14, -- necessary: percentage of getting this role selected (per player)
-	maximum = 1, -- maximum amount of roles in a round
-	minPlayers = 7, -- minimum amount of players until this role is able to get selected
-	credits = 3, -- the starting credits of a specific role
-	togglable = true, -- option to toggle a role for a client if possible (F1 menu)
-	random = 50
-}
+function ROLE:PreInitialize()
+	self.color = Color(100, 190, 205, 255) -- ...
+	self.dkcolor = Color(36, 134, 152, 255) -- ...
+	self.bgcolor = Color(255, 188, 121, 255) -- ...
+	self.abbr = "jack" -- abbreviation
+	self.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
+	self.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
+	self.scoreTeamKillsMultiplier = -8 -- multiplier for teamkill
+	self.fallbackTable = {}
+	self.traitorCreditAward = true -- will receive credits on kill like a traitor
+	
+	self.defaultTeam = TEAM_JACKAL -- the team name: roles with same team name are working together
+	self.defaultEquipment = JACKAL_EQUIPMENT -- here you can set up your own default equipment
+	
+	self.conVarData = {
+		pct = 0.14, -- necessary: percentage of getting this role selected (per player)
+		maximum = 1, -- maximum amount of roles in a round
+		minPlayers = 7, -- minimum amount of players until this role is able to get selected
+		credits = 3, -- the starting credits of a specific role
+		togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+		random = 50
+	}
+end
 
 -- init jackal fallback table
 hook.Add("InitFallbackShops", "JackInitFallback", function()
@@ -40,43 +41,43 @@ hook.Add("InitFallbackShops", "JackInitFallback", function()
 	InitFallbackShop(JACKAL, table.Merge(JACKAL.fallbackTable, TRAITOR.fallbackTable)) -- merge jackal equipment with traitor equipment
 end)
 
-hook.Add("TTT2FinishedLoading", "JackInitT", function()
+function ROLE:Initialize()
 	if SERVER and JESTER then
 		-- add a easy role filtering to receive all jesters
 		-- but just do it, when the role was created, then update it with recommended function
 		-- theoretically this function is not necessary to call, but maybe there are some modifications
 		-- of other addons. So it's better to use this function
 		-- because it calls hooks and is doing some networking
-		JACKAL.networkRoles = {JESTER}
+		self.networkRoles = {JESTER}
 	end
 
 	if CLIENT then
 		-- Role specific language elements
-		LANG.AddToLanguage("English", JACKAL.name, "Jackal")
-		LANG.AddToLanguage("English", TEAM_JACKAL, "TEAM Jackal")
-		LANG.AddToLanguage("English", "info_popup_" .. JACKAL.name, [[You are the Jackal! Try to kill everyone but you! Make use of your sidekick deagle to shoot yourself a partner in crime.]])
-		LANG.AddToLanguage("English", "body_found_" .. JACKAL.abbr, "They were a Jackal!")
-		LANG.AddToLanguage("English", "search_role_" .. JACKAL.abbr, "This person was a Jackal!")
-		LANG.AddToLanguage("English", "target_" .. JACKAL.name, "Jackal")
-		LANG.AddToLanguage("English", "ttt2_desc_" .. JACKAL.name, [[The Jackal needs to win alone or with his sidekick!]])
-		LANG.AddToLanguage("English", "hilite_win_" .. TEAM_JACKAL, "THE JACK WON")
-		LANG.AddToLanguage("English", "win_" .. TEAM_JACKAL, "The Jackal has won!")
-		LANG.AddToLanguage("English", "ev_win_" .. TEAM_JACKAL, "The evil Jackal won the round!")
-		LANG.AddToLanguage("English", "credit_" .. JACKAL.abbr .. "_all", "Jackals, you have been awarded {num} equipment credit(s) for your performance.")
+		LANG.AddToLanguage("English", self.name, "Jackal")
+		LANG.AddToLanguage("English", self.defaultTeam, "TEAM Jackal")
+		LANG.AddToLanguage("English", "info_popup_" .. self.name, [[You are the Jackal! Try to kill everyone but you! Make use of your sidekick deagle to shoot yourself a partner in crime.]])
+		LANG.AddToLanguage("English", "body_found_" .. self.abbr, "They were a Jackal!")
+		LANG.AddToLanguage("English", "search_role_" .. self.abbr, "This person was a Jackal!")
+		LANG.AddToLanguage("English", "target_" .. self.name, "Jackal")
+		LANG.AddToLanguage("English", "ttt2_desc_" .. self.name, [[The Jackal needs to win alone or with his sidekick!]])
+		LANG.AddToLanguage("English", "hilite_win_" .. self.defaultTeam, "THE JACK WON")
+		LANG.AddToLanguage("English", "win_" .. self.defaultTeam, "The Jackal has won!")
+		LANG.AddToLanguage("English", "ev_win_" .. self.defaultTeam, "The evil Jackal won the round!")
+		LANG.AddToLanguage("English", "credit_" .. self.abbr .. "_all", "Jackals, you have been awarded {num} equipment credit(s) for your performance.")
 
-		LANG.AddToLanguage("Deutsch", JACKAL.name, "Schackal")
-		LANG.AddToLanguage("Deutsch", TEAM_JACKAL, "TEAM Schackal")
-		LANG.AddToLanguage("Deutsch", "info_popup_" .. JACKAL.name, [[Du bist ein Schackal! Versuche jeden anderen Spieler zu töten! Nutze deine Sidekickdeagle, um dir einen Komplizen zu schießen.]])
-		LANG.AddToLanguage("Deutsch", "body_found_" .. JACKAL.abbr, "Er war ein Schackal!")
-		LANG.AddToLanguage("Deutsch", "search_role_" .. JACKAL.abbr, "Diese Person war ein Schackal!")
-		LANG.AddToLanguage("Deutsch", "target_" .. JACKAL.name, "Schackal")
-		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. JACKAL.name, [[Der Schackal muss alleine oder mit seinem Sidekick gewinnen!]])
-		LANG.AddToLanguage("Deutsch", "hilite_win_" .. TEAM_JACKAL, "THE JACK WON")
-		LANG.AddToLanguage("Deutsch", "win_" .. TEAM_JACKAL, "Der Schackal hat gewonnen!")
-		LANG.AddToLanguage("Deutsch", "ev_win_" .. TEAM_JACKAL, "Der böse Schackal hat die Runde gewonnen!")
-		LANG.AddToLanguage("Deutsch", "credit_" .. JACKAL.abbr .. "_all", "Schackale, euch wurden {num} Ausrüstungs-Credit(s) für eure Leistung gegeben.")
+		LANG.AddToLanguage("Deutsch", self.name, "Schackal")
+		LANG.AddToLanguage("Deutsch", self.defaultTeam, "TEAM Schackal")
+		LANG.AddToLanguage("Deutsch", "info_popup_" .. self.name, [[Du bist ein Schackal! Versuche jeden anderen Spieler zu töten! Nutze deine Sidekickdeagle, um dir einen Komplizen zu schießen.]])
+		LANG.AddToLanguage("Deutsch", "body_found_" .. self.abbr, "Er war ein Schackal!")
+		LANG.AddToLanguage("Deutsch", "search_role_" .. self.abbr, "Diese Person war ein Schackal!")
+		LANG.AddToLanguage("Deutsch", "target_" .. self.name, "Schackal")
+		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. self.name, [[Der Schackal muss alleine oder mit seinem Sidekick gewinnen!]])
+		LANG.AddToLanguage("Deutsch", "hilite_win_" .. self.defaultTeam, "THE JACK WON")
+		LANG.AddToLanguage("Deutsch", "win_" .. self.defaultTeam, "Der Schackal hat gewonnen!")
+		LANG.AddToLanguage("Deutsch", "ev_win_" .. self.defaultTeam, "Der böse Schackal hat die Runde gewonnen!")
+		LANG.AddToLanguage("Deutsch", "credit_" .. self.abbr .. "_all", "Schackale, euch wurden {num} Ausrüstungs-Credit(s) für eure Leistung gegeben.")
 	end
-end)
+end
 
 if SERVER then	
 	-- modify roles table of rolesetup addon
