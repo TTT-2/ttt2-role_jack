cd3cee1e422e5af2368d2019e5eb1d95ed14f17a
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-05-31 13:17:23 +0200

    Added Italian translation (#4)

---
+++ lua/lang/italiano/jackal.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "Jackal"
+L[JACKAL.defaultTeam] = "TEAM Jackal"
+L["hilite_win_" .. JACKAL.defaultTeam] = "I JACKAL HANNO VINTO"
+L["win_" .. JACKAL.defaultTeam] = "I Jackal hanno vinto!"
+L["info_popup_" .. JACKAL.name] = [[Sei il Jackal! Prova ad uccidere tutti tranne te!
+Usa la tua Sidekick Deagle per colpire qualcuno e farlo diventare il tuo Sidekick.]]
+L["body_found_" .. JACKAL.abbr] = "Era un Jackal!"
+L["search_role_" .. JACKAL.abbr] = "Questa persona era un Jackal!"
+L["ev_win_" .. JACKAL.defaultTeam] = "Il malvagio Jackal ha vinto il round!"
+L["target_" .. JACKAL.name] = "Jackal"
+L["ttt2_desc_" .. JACKAL.name] = [[Il Jackal deve vincere da solo o con il suo Sidekick!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Jackal, vi sono stati dati {num} credito/i per la vostra performance."
