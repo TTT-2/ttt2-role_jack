c0ba3cf848ebd8db0a98181e9fdf98411cf02231
Tekiad <tekiadp@gmail.cm>
2020-08-15 12:37:14 -0300

    Create jackal.lua (#5)

---
+++ lua/lang/spanish/jackal.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("Español")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "Zorro"
+L[JACKAL.defaultTeam] = "EQUIPO Zorros"
+L["hilite_win_" .. JACKAL.defaultTeam] = "EL ZORRO GANA"
+L["win_" .. JACKAL.defaultTeam] = "¡El Zorro ha ganado!"
+L["info_popup_" .. JACKAL.name] = [[Eres el Zorro! Te equipo empieza contigo, pero puede terminar en muchos.
+Usa tu Sidekick Deagle para aliarte con otras almas solitarias.]]
+L["body_found_" .. JACKAL.abbr] = "¡Era un Zorro!"
+L["search_role_" .. JACKAL.abbr] = "Esta persona era un Zorro."
+L["ev_win_" .. JACKAL.defaultTeam] = "¡El solitario Zorro gana esta ronda!"
+L["target_" .. JACKAL.name] = "Zorro"
+L["ttt2_desc_" .. JACKAL.name] = [[El Zorro gana como caza, solo. Sin embargo, puede hacerse unos aliados en el camino.]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Zorros, han sido recompensados con {num} crédito(s) por su desempeño."
