9d379f63e5c512c3f78a80bb6beaab6b8309285c
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 21:41:00 +0500

    Create jackal.lua

---
+++ lua/terrortown/lang/russian/jackal.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "Шакал"
+L[JACKAL.defaultTeam] = "Команда шакалов"
+L["hilite_win_" .. JACKAL.defaultTeam] = "ПОБЕДА ШАКАЛОВ"
+L["win_" .. JACKAL.defaultTeam] = "Шакалы победили!"
+L["info_popup_" .. JACKAL.name] = [[Вы шакал! Попробуй убить всех, кроме тебя!
+Используйте свой deagle помощника, чтобы пристрелить соучастника.]]
+L["body_found_" .. JACKAL.abbr] = "Он был шакалом!"
+L["search_role_" .. JACKAL.abbr] = "Этот человек был шакалом!"
+L["ev_win_" .. JACKAL.defaultTeam] = "Злые Шакалы победили!"
+L["target_" .. JACKAL.name] = "Шакал"
+L["ttt2_desc_" .. JACKAL.name] = [[Шакалу нужно побеждать в одиночку или со своим сообщником!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Шакалы, за вашу работу вы получили {num} кредит(а/ов)."
