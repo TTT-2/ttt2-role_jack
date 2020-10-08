6bc4e1176fac232974ee00769d2856a77bdc16dc
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-08 18:04:20 +0200

    Update italian

--- lua/terrortown/lang/italiano/jackal.lua
+++ lua/terrortown/lang/italiano/jackal.lua
@@ -1,15 +1,15 @@
 L = LANG.GetLanguageTableReference("italiano")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[JACKAL.name] = "Jackal"
-L[JACKAL.defaultTeam] = "Team Jackal"
-L["hilite_win_" .. JACKAL.defaultTeam] = "I JACKAL HANNO VINTO"
-L["win_" .. JACKAL.defaultTeam] = "I Jackal hanno vinto!"
-L["info_popup_" .. JACKAL.name] = [[Sei il Jackal! Prova ad uccidere tutti tranne te!
-Usa la tua Sidekick Deagle per colpire qualcuno e farlo diventare il tuo Sidekick.]]
-L["body_found_" .. JACKAL.abbr] = "Era un Jackal!"
-L["search_role_" .. JACKAL.abbr] = "Questa persona era un Jackal!"
-L["ev_win_" .. JACKAL.defaultTeam] = "Il malvagio Jackal ha vinto il round!"
-L["target_" .. JACKAL.name] = "Jackal"
-L["ttt2_desc_" .. JACKAL.name] = [[Il Jackal deve vincere da solo o con il suo Sidekick!]]
-L["credit_" .. JACKAL.abbr .. "_all"] = "Jackal, vi sono stati dati {num} credito/i per la vostra performance."
+L[JACKAL.name] = "Sciacallo"
+L[JACKAL.defaultTeam] = "Team Sciacalli"
+L["hilite_win_" .. JACKAL.defaultTeam] = "I SCIACALLI HANNO VINTO"
+L["win_" .. JACKAL.defaultTeam] = "I Sciacallo hanno vinto!"
+L["info_popup_" .. JACKAL.name] = [[Sei uno Sciacallo! Prova ad uccidere tutti tranne te!
+Usa la tua Deagle Aiutante per colpire qualcuno e farlo diventare il tuo Aiutante.]]
+L["body_found_" .. JACKAL.abbr] = "Era uno Sciacallo!"
+L["search_role_" .. JACKAL.abbr] = "Questa persona era un Sciacallo!"
+L["ev_win_" .. JACKAL.defaultTeam] = "Il malvagio Sciacallo ha vinto il round!"
+L["target_" .. JACKAL.name] = "Sciacallo"
+L["ttt2_desc_" .. JACKAL.name] = [[lo Sciacallo deve vincere da solo o con il suo Aiutante!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Sciacalli, vi sono stati dati {num} credito/i per la vostra performance."
