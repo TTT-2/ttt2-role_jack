7f73b53bc9065676b7a7b73c44f4fb6debd3d7b5
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:56:05 +0100

    Create jackal.lua (#8)
    french translation :Jackal
    
    Co-authored-by: Alf21 <4lf-mueller@gmx.de>

---
+++ lua/terrortown/lang/french/jackal.lua
@@ -0,0 +1,15 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "Chacal"
+L[JACKAL.defaultTeam] = "Team Chacal"
+L["hilite_win_" .. JACKAL.defaultTeam] = "LE CHACAL A GAGNÉ"
+L["win_" .. JACKAL.defaultTeam] = "Le Chacal a gagné!"
+L["info_popup_" .. JACKAL.name] = [[Vous êtes le Chacal! Essayez de tuer tout le monde sauf vous !
+Utilisez votre deagle de l'acolyte  pour  tirer une balle dans la tête de votre futur compagnons.]]
+L["body_found_" .. JACKAL.abbr] = "C'était un Chacal!"
+L["search_role_" .. JACKAL.abbr] = "Cette personne était un Chacal!"
+L["ev_win_" .. JACKAL.defaultTeam] = "Le méchant Chacal a gagné la manche!"
+L["target_" .. JACKAL.name] = "Chacal"
+L["ttt2_desc_" .. JACKAL.name] = [[Le Chacal doit gagner seul ou avec ses Acolyte!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Chacal, vous avez été récompensé de {num} crédit(s) d'équipement pour votre performances."
