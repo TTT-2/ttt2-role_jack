7c8b99e3bce4e89fd48688c89d06f7ab66b75faa
westooooo <68987820+westooooo@users.noreply.github.com>
2021-03-08 15:25:31 +0900

    Add files via upload

---
+++ lua/terrortown/lang/japanese/jackal.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("japanese")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "Jackal"
+L[JACKAL.defaultTeam] = "Jackal陣営"
+L["hilite_win_" .. JACKAL.defaultTeam] = "Jackal陣営の勝利"
+L["win_" .. JACKAL.defaultTeam] = "Jackalの勝利！"
+L["info_popup_" .. JACKAL.name] = [[あなたはJackalだ！自分以外の全員を殺せば勝ちだ！
+Sidekick Deagleで誰かに撃ち、仲間を作り出そう！]]
+L["body_found_" .. JACKAL.abbr] = "奴はJackalだったな！"
+L["search_role_" .. JACKAL.abbr] = "こいつはJackalだったな！"
+L["ev_win_" .. JACKAL.defaultTeam] = "漁夫の利好きなJackalが勝利した！"
+L["target_" .. JACKAL.name] = "Jackal"
+L["ttt2_desc_" .. JACKAL.name] = [[Jackalは単独か、Sidekickとともに生き残れば勝ちだ！]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Jackalよ、任務遂行により、{num}個のクレジットを受け取ったぞ。"
