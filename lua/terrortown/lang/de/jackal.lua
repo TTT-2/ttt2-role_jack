5cff44118a061ba7baadd5d54723ffecf5154ae9
Tim Goll <tim@timgoll.de>
2021-06-07 14:22:48 +0200

    fixed german translation for Jackal

--- lua/terrortown/lang/de/jackal.lua
+++ lua/terrortown/lang/de/jackal.lua
@@ -1,15 +1,15 @@
 L = LANG.GetLanguageTableReference("de")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[JACKAL.name] = "Schackal"
-L[JACKAL.defaultTeam] = "Team Schackal"
+L[JACKAL.name] = "Schakal"
+L[JACKAL.defaultTeam] = "Team Schakal"
 L["hilite_win_" .. JACKAL.defaultTeam] = "TEAM SCHAKAL GEWANN"
-L["win_" .. JACKAL.defaultTeam] = "Der Schackal hat gewonnen!"
-L["info_popup_" .. JACKAL.name] = [[Du bist ein Schackal! Versuche jeden anderen Spieler zu töten!
+L["win_" .. JACKAL.defaultTeam] = "Der Schakal hat gewonnen!"
+L["info_popup_" .. JACKAL.name] = [[Du bist ein Schakal! Versuche jeden anderen Spieler zu töten!
 Nutze deine Sidekickdeagle, um dir einen Komplizen zu schießen.]]
-L["body_found_" .. JACKAL.abbr] = "Er war ein Schackal!"
-L["search_role_" .. JACKAL.abbr] = "Diese Person war ein Schackal!"
-L["ev_win_" .. JACKAL.defaultTeam] = "Der böse Schackal hat die Runde gewonnen!"
-L["target_" .. JACKAL.name] = "Schackal"
-L["ttt2_desc_" .. JACKAL.name] = [[Der Schackal muss alleine oder mit seinem Sidekick gewinnen!]]
-L["credit_" .. JACKAL.abbr .. "_all"] = "Schackale, euch wurden {num} Ausrüstungs-Credit(s) für eure Leistung gegeben."
+L["body_found_" .. JACKAL.abbr] = "Er war ein Schakal!"
+L["search_role_" .. JACKAL.abbr] = "Diese Person war ein Schakal!"
+L["ev_win_" .. JACKAL.defaultTeam] = "Der böse Schakal hat die Runde gewonnen!"
+L["target_" .. JACKAL.name] = "Schakal"
+L["ttt2_desc_" .. JACKAL.name] = [[Der Schakal muss alleine oder mit seinem Sidekick gewinnen!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Schakale, euch wurden {num} Ausrüstungs-Credit(s) für eure Leistung gegeben."
