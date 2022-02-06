c19ee5a2d562a348ec5175da4640e7d9cbecf9ec
Carlos Luque Córdoba <carlosluque6@gmail.com>
2022-02-07 00:13:12 +0100

    Update jackal.lua

--- lua/terrortown/lang/es/jackal.lua
+++ lua/terrortown/lang/es/jackal.lua
@@ -1,15 +1,15 @@
 L = LANG.GetLanguageTableReference("es")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[JACKAL.name] = "Zorro"
-L[JACKAL.defaultTeam] = "Equipo Zorros"
-L["hilite_win_" .. JACKAL.defaultTeam] = "EL ZORRO GANA"
-L["win_" .. JACKAL.defaultTeam] = "¡El Zorro ha ganado!"
-L["info_popup_" .. JACKAL.name] = [[Eres el Zorro! Te equipo empieza contigo, pero puede terminar en muchos.
+L[JACKAL.name] = "Chacal"
+L[JACKAL.defaultTeam] = "Equipo Chacales"
+L["hilite_win_" .. JACKAL.defaultTeam] = "EL Chacal GANA"
+L["win_" .. JACKAL.defaultTeam] = "¡El Chacal ha ganado!"
+L["info_popup_" .. JACKAL.name] = [[Eres el Chacal! Te equipo empieza contigo, pero puede terminar en muchos.
 Usa tu Sidekick Deagle para aliarte con otras almas solitarias.]]
-L["body_found_" .. JACKAL.abbr] = "¡Era un Zorro!"
-L["search_role_" .. JACKAL.abbr] = "Esta persona era un Zorro."
-L["ev_win_" .. JACKAL.defaultTeam] = "¡El solitario Zorro gana esta ronda!"
-L["target_" .. JACKAL.name] = "Zorro"
-L["ttt2_desc_" .. JACKAL.name] = [[El Zorro gana como caza, solo. Sin embargo, puede hacerse unos aliados en el camino.]]
-L["credit_" .. JACKAL.abbr .. "_all"] = "Zorros, han sido recompensados con {num} crédito(s) por su desempeño."
+L["body_found_" .. JACKAL.abbr] = "¡Era un Chacal!"
+L["search_role_" .. JACKAL.abbr] = "Esta persona era un Chacal."
+L["ev_win_" .. JACKAL.defaultTeam] = "¡El solitario Chacal gana esta ronda!"
+L["target_" .. JACKAL.name] = "Chacal"
+L["ttt2_desc_" .. JACKAL.name] = [[El Chacal gana como caza, solo. Sin embargo, puede hacerse unos aliados en el camino.]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Chacales, han sido recompensados con {num} crédito(s) por su desempeño."
