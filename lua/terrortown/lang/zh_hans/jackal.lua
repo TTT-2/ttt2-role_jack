e8af30ea8f5ac5b73111d988f7c426f4a34fd172
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-09-03 00:45:18 +0800

    Add simplified Chinese Translation

---
+++ lua/terrortown/lang/zh_hans/jackal.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("zh_hans")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "豺狼"
+L[JACKAL.defaultTeam] = "豺狼队伍"
+L["hilite_win_" .. JACKAL.defaultTeam] = "豺狼队伍获胜"
+L["win_" .. JACKAL.defaultTeam] = "豺狼获胜!"
+L["info_popup_" .. JACKAL.name] = [[你是豺狼!试着杀死所有人,除了你!
+利用你的伙伴迪格尔开枪打死你自己,一个犯罪伙伴.]]
+L["body_found_" .. JACKAL.abbr] = "他们是豺狼!"
+L["search_role_" .. JACKAL.abbr] = "这个人是豺狼!"
+L["ev_win_" .. JACKAL.defaultTeam] = "邪恶的豺狼赢了这一回合!"
+L["target_" .. JACKAL.name] = "豺狼"
+L["ttt2_desc_" .. JACKAL.name] = [[豺狼需要单独或与同伴一起获胜!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "豺狼,您的表现已获得{num}个设备积分."
