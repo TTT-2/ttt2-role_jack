cbff71b3865ad522a6cd7ec7171663fd2b71964d
Talha Berkay Akbulut <49911006+NovaDiablox@users.noreply.github.com>
2023-11-16 16:31:58 +0300

    Added Turkish Language
    Added Turkish Language

---
+++ lua/terrortown/lang/tr/jackal.lua
@@ -0,0 +1,19 @@
+local L = LANG.GetLanguageTableReference("tr")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JACKAL.name] = "Çakal"
+L[JACKAL.defaultTeam] = "Çakal Takımı"
+L["hilite_win_" .. JACKAL.defaultTeam] = "ÇAKAL TAKIMI KAZANDI"
+L["win_" .. JACKAL.defaultTeam] = "Çakallar kazandı!"
+L["info_popup_" .. JACKAL.name] = [[Çakalsın! Kendin hariç herkesi öldürmeye çalış!
+Kendinizi suç ortağı olarak vurmak için yardımcı Deagle'ınızı kullanın.]]
+L["body_found_" .. JACKAL.abbr] = "Onlar bir Çakaldı!"
+L["search_role_" .. JACKAL.abbr] = "Bu kişi bir Çakaldı!"
+L["ev_win_" .. JACKAL.defaultTeam] = "Kötü Çakal raundu kazandı!"
+L["target_" .. JACKAL.name] = "Çakal"
+L["ttt2_desc_" .. JACKAL.name] = [[Çakalın tek başına veya yardımcısı ile kazanması gerekiyor!]]
+L["credit_" .. JACKAL.abbr .. "_all"] = "Çakallar, performansınız için size {num} ekipman kredisi verildi."
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["label_jackal_armor_value"] = "Canlanırken zırh değeri"
+L["label_jackal_spawn_siki_deagle"] = "Bir yardımcı deagle ile canlanmayı etkinleştir"
